0b120e44a87e6cd2025b3ccceadcec1b6a4dc800
Paulo Henrique S. Goes <44791413+phsgoes@users.noreply.github.com>
2019-07-06 19:03:52 -0300

    Update UpdateCompany.js

--- server/app/Validators/UpdateCompany.js
+++ server/app/Validators/UpdateCompany.js
@@ -3,15 +3,15 @@
 class UpdateCompany {
   get rules () {
     return {
-      razao_social: 'required|min:5|unique:companies,razao_social'
+      // razao_social: 'required|min:5|unique:companies,razao_social'
     }
   }
 
   get messages () {
     return {
-      'razao_social.required': 'Razão social é obrigatório',
-      'razao_social.min': 'Razão social deve ter pelo menos 5 caracteres',
-      'razao_social.unique': 'Razão social duplicada'
+      // 'razao_social.required': 'Razão social é obrigatório',
+      // 'razao_social.min': 'Razão social deve ter pelo menos 5 caracteres',
+      // 'razao_social.unique': 'Razão social duplicada'
     }
   }
 }
